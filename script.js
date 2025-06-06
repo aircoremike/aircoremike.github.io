cca8c17c09f17ba99fce5d33b2fd8ec1bcda5b4b
aircoremike <mike@aircorebearings.com>
2025-06-06 10:28:21 +0800

    refactor with no header

--- script.js
+++ script.js
@@ -7,16 +7,13 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  // Navbar logo and header shrink on scroll
+  // Navbar logo and navbar shrink on scroll
   const navbar = document.querySelector('.navbar');
-  const header = document.querySelector('header');
   function handleNavbarShrink() {
     if (window.scrollY > 10) {
       navbar.classList.add('scrolled');
-      header.classList.add('scrolled');
     } else {
       navbar.classList.remove('scrolled');
-      header.classList.remove('scrolled');
     }
   }
   window.addEventListener('scroll', handleNavbarShrink);
